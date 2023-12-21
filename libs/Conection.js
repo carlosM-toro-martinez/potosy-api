c6fc6b8f63edb387a21182489ea728a2c6d2f086
carlos mario toro martinez <carlostorom.96@gmail.com>
2023-12-21 10:18:37 -0400

    fix: ssl certificate false in database

--- libs/Conection.js
+++ libs/Conection.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 const { config } = require('../config/config');
 
-const URI = `postgres://${config.dbUser}:${config.dbPass}@${config.dbHost}:${config.dbPort}/${config.dbName}?ssl=true`;
+const URI = `postgres://${config.dbUser}:${config.dbPass}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 // const URI = `postgres://carlostoro:sI7BJq2RV9ivU07AEoPzZG0xTuAUN9Pz@dpg-cf2panarrk0bppcc9cg0-a.oregon-postgres.render.com/my_store_z87a?ssl=true`;
 
 const pool = new Pool({ connectionString: URI });
